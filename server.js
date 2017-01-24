cef47972ec006b8334c4555309e0de52ebbb7ec1
ahmmdd <ahmedmohammedjuned@gmail.com>
2017-01-24 12:46:34 -0500

    Express server configured

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
 // Our Express Server
+//Imported third party module for epress
+let express = require('express');
+
+//created server object
+let app = express();
+
+//assign a port to constant
+const port = 3000;
+// About route
+app.get('/about', (req, res) =>{
+  res.send('About Us');
+});
+
+// Contact route
+app.get('/contact', (req, res) =>{
+  res.send('Contact Us');
+});
+
+// main root for any website
+app.get('/', (req, res) =>{
+  res.send('Welcome');
+});
+
+// setup event loop 
+app.listen(3000, () =>{
+  console.log(`Server running at http://localhost: ${port}`);
+});
